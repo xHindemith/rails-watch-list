87ca5fa545d28cfee5df44b7e5945d10ae54f0f6
Kostas Piludis <kostas.piludis@gmail.com>
2023-08-11 12:57:53 +0900

    added cloudinary config and from input

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   validates :name, uniqueness: true
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_one_attached :photo
 end
